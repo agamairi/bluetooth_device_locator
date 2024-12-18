bf66d54c4d19f5fd32bec9bae247b249d95b0f60
agamairi <agam.airi@gmail.com>
2024-12-17 22:55:44 -0500

    adding filter on scan to only show previously paired devices only

--- lib/services/ble_service.dart
+++ lib/services/ble_service.dart
@@ -6,21 +6,29 @@ class BleServices {
   List<BluetoothDevice> devicesList = [];
   bool isScanning = false;
 
-  // Start scanning for devices
+  // Get the list of paired devices
+  Future<List<BluetoothDevice>> getPairedDevices() async {
+    return await FlutterBluePlus.bondedDevices;
+  }
+
+  // Start scanning for previously paired devices only
   Future<void> startScan() async {
-    // Start scanning with the desired filters
+    // Get the list of paired devices
+    List<BluetoothDevice> pairedDevices = await getPairedDevices();
+
+    // Start scanning with the desired filters, if any paired devices are found
     await FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
 
-    // Listen to scan results
     FlutterBluePlus.scanResults.listen((results) {
       for (ScanResult result in results) {
-        if (!devicesList.contains(result.device)) {
+        // Check if the scanned device is in the list of paired devices
+        if (pairedDevices.contains(result.device) &&
+            !devicesList.contains(result.device)) {
           devicesList.add(result.device);
         }
       }
     });
 
-    // Listen to scanning status
     FlutterBluePlus.isScanning.listen((isScanning) {
       this.isScanning = isScanning;
     });
