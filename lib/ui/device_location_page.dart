085f816ced515f2a5750c08945ecb63a9bc3479c
agamairi <agam.airi@gmail.com>
2024-12-17 22:11:48 -0500

    creating basic ui that would keep a track of the device location

--- lib/ui/device_location_page.dart
+++ lib/ui/device_location_page.dart
@@ -1,3 +1,4 @@
+import 'package:ble_locator/services/location_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
@@ -7,11 +8,18 @@ class DeviceLocationPage extends StatefulWidget {
   const DeviceLocationPage({super.key, required this.device});
 
   @override
-  _DeviceLocationPageState createState() => _DeviceLocationPageState();
+  State<DeviceLocationPage> createState() => _DeviceLocationPageState();
 }
 
 class _DeviceLocationPageState extends State<DeviceLocationPage> {
   int rssiValue = 0;
+  int previousRssiValue = 0;
+
+  // Create an instance of LocationService
+  final LocationService _locationService = LocationService();
+
+  // Movement feedback
+  String _movementFeedback = 'Start walking toward the device';
 
   @override
   void initState() {
@@ -19,33 +27,165 @@ class _DeviceLocationPageState extends State<DeviceLocationPage> {
     startRssiReading();
   }
 
+  // Start reading RSSI values from the Bluetooth device
   void startRssiReading() async {
     while (true) {
       await widget.device.readRssi().then((value) {
         setState(() {
+          previousRssiValue = rssiValue;
           rssiValue = value;
+
+          // Apply Kalman filter to smooth RSSI value
+          _locationService.applyKalmanFilter(rssiValue.toDouble());
         });
       });
-      await Future.delayed(
-          const Duration(seconds: 1)); // Adjust the interval as needed
+      await Future.delayed(const Duration(seconds: 1));
     }
+  }
+
+  // Calculate the distance from the device using the LocationService
+  num get estimatedDistance =>
+      _locationService.calculateDistanceFromRssi(rssiValue);
+
+  // Track movement direction using the LocationService
+  void _trackMovement() {
+    setState(() {
+      _movementFeedback =
+          _locationService.trackMovementDirection(rssiValue, previousRssiValue);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // Smoothed RSSI value
+    int smoothedRssi = _locationService.smoothedRssi.toInt();
+
+    // Track movement based on RSSI changes
+    _trackMovement();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Device Location'),
+        title: const Text('Device Location'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Device Name: ${widget.device.name}'),
-            Text('Device Address: ${widget.device.id}'),
-            Text('RSSI: $rssiValue'),
-            // Add navigation instructions based on RSSI and other sensor data
-            Text('Navigation Instruction: Move closer to the device'),
+            // Device Info Card
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 5,
+              color: Colors.blue.shade50,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Device: ${widget.device.name}',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Address: ${widget.device.id.id}',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // RSSI Progress Bar
+                    LinearProgressIndicator(
+                      value:
+                          (rssiValue + 100) / 100, // Normalize RSSI (-100 to 0)
+                      backgroundColor: Colors.grey.shade300,
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                          rssiValue > previousRssiValue
+                              ? Colors.green
+                              : Colors.red),
+                      minHeight: 8,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Signal Strength: ${rssiValue} dBm',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Smoothed RSSI: $smoothedRssi dBm',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Estimated Distance: ${estimatedDistance.toStringAsFixed(2)} meters',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            // Movement Feedback Section
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.green.shade50,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Movement Feedback:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green.shade700,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _movementFeedback,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.green.shade700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            // Arrow indicating movement direction (optional)
+            Icon(
+              Icons.arrow_forward,
+              size: 100,
+              color: Colors.blueAccent,
+            ),
           ],
         ),
       ),
