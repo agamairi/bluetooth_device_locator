a6756bb4cf014588ac271658d15cfc6f50cb1e30
agamairi <agam.airi@gmail.com>
2024-12-17 22:16:35 -0500

    refactoring based on the new ble service class

--- lib/ui/device_list_screen.dart
+++ lib/ui/device_list_screen.dart
@@ -1,6 +1,8 @@
-import 'package:ble_locator/ui/device_location_page.dart';
+// lib/ui/device_location_page.dart
+import 'package:ble_locator/services/ble_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'device_location_page.dart';
 
 class DeviceDiscoveryPage extends StatefulWidget {
   const DeviceDiscoveryPage({super.key});
@@ -10,59 +12,39 @@ class DeviceDiscoveryPage extends StatefulWidget {
 }
 
 class _DeviceDiscoveryPageState extends State<DeviceDiscoveryPage> {
-  FlutterBluePlus flutterBlue = FlutterBluePlus();
-  List<BluetoothDevice> devicesList = [];
+  final BleServices _bleServices =
+      BleServices(); // Create an instance of BleServices
   late BluetoothDevice connectedDevice;
-  bool isScanning = false;
 
   @override
   void initState() {
     super.initState();
-    startScan();
+    _startScan();
   }
 
   // Start scanning for devices
-  void startScan() async {
-    // Start scanning with the desired filters
-    await FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
-
-    FlutterBluePlus.scanResults.listen((results) {
-      for (ScanResult result in results) {
-        if (!devicesList.contains(result.device)) {
-          setState(() {
-            devicesList.add(result.device);
-          });
-        }
-      }
-    });
-
-    FlutterBluePlus.isScanning.listen((isScanning) {
-      setState(() {
-        this.isScanning = isScanning;
-      });
-    });
+  void _startScan() async {
+    await _bleServices.startScan();
+    setState(() {});
   }
 
   // Stop scanning for devices
-  void stopScan() async {
-    await FlutterBluePlus.stopScan();
+  void _stopScan() async {
+    await _bleServices.stopScan();
+    setState(() {});
   }
 
   // Connect to a device
-  Future<void> connectToDevice(BluetoothDevice device) async {
-    try {
-      await device.connect();
-      setState(() {
-        connectedDevice = device;
-      });
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+    bool success = await _bleServices.connectToDevice(device);
+    if (success) {
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => DeviceLocationPage(device: device),
         ),
       );
-    } catch (e) {
-      print("Error connecting to device: $e");
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Failed to connect to device')),
       );
@@ -71,7 +53,7 @@ class _DeviceDiscoveryPageState extends State<DeviceDiscoveryPage> {
 
   @override
   void dispose() {
-    stopScan();
+    _stopScan();
     super.dispose();
   }
 
@@ -82,25 +64,26 @@ class _DeviceDiscoveryPageState extends State<DeviceDiscoveryPage> {
         title: const Text('Device Discovery'),
         actions: [
           IconButton(
-              onPressed: () {
-                stopScan();
-                startScan();
-              },
-              icon: const Icon(Icons.refresh))
+            onPressed: () {
+              _stopScan();
+              _startScan();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
         ],
       ),
       body: Column(
         children: [
-          if (isScanning)
+          if (_bleServices.isScanning)
             const Padding(
               padding: EdgeInsets.all(8.0),
               child: CircularProgressIndicator(),
             ),
           Expanded(
             child: ListView.builder(
-              itemCount: devicesList.length,
+              itemCount: _bleServices.devicesList.length,
               itemBuilder: (context, index) {
-                BluetoothDevice device = devicesList[index];
+                BluetoothDevice device = _bleServices.devicesList[index];
                 return Card(
                   child: ListTile(
                     title: Text(device.advName.isEmpty
@@ -108,7 +91,7 @@ class _DeviceDiscoveryPageState extends State<DeviceDiscoveryPage> {
                         : device.advName),
                     subtitle: Text(device.remoteId.toString()),
                     trailing: ElevatedButton(
-                      onPressed: () => connectToDevice(device),
+                      onPressed: () => _connectToDevice(device),
                       child: const Text('Connect'),
                     ),
                   ),
