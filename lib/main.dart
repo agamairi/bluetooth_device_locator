e0e6967822034cb60e32e8d646154d92a63de616
agamairi <agam.airi@gmail.com>
2024-12-17 23:16:55 -0500

    adding app theme to the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ble_locator/ui/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'ui/device_list_screen.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BLE Messaging App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: buildAppTheme(),
       home:
           const DeviceDiscoveryPage(), // Initial screen showing scanned devices
     );
