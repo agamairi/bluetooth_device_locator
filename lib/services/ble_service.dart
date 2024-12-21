c2a4f9af3e1a7afc9f60228d35c4e45bf604ddba
agamairi <agam.airi@gmail.com>
2024-12-21 04:44:28 -0500

    adding check for connected devices

--- lib/services/ble_service.dart
+++ lib/services/ble_service.dart
@@ -11,33 +11,38 @@ class BleServices {
     return await FlutterBluePlus.bondedDevices;
   }
 
+  Future<List<BluetoothDevice>> getConnectedDevices() async {
+    return FlutterBluePlus.connectedDevices;
+  }
+
   // Start scanning for previously paired devices only
   Future<void> startScan() async {
+    isScanning = true;
     // Get the list of paired devices
     List<BluetoothDevice> pairedDevices = await getPairedDevices();
-
-    await FlutterBluePlus.stopScan();
+    List<BluetoothDevice> connectedDevices = await getConnectedDevices();
 
     // Start scanning with the desired filters, if any paired devices are found
     await FlutterBluePlus.startScan(timeout: const Duration(seconds: 4));
+    await Future.delayed(const Duration(seconds: 4), () {
+      FlutterBluePlus.stopScan();
+    });
 
     FlutterBluePlus.scanResults.listen((results) {
       for (ScanResult result in results) {
         // Check if the scanned device is in the list of paired devices
-        if (pairedDevices.contains(result.device) &&
+        if ((pairedDevices.contains(result.device) ||
+                connectedDevices.contains(result.device)) &&
             !devicesList.contains(result.device)) {
           devicesList.add(result.device);
         }
       }
     });
-
-    FlutterBluePlus.isScanning.listen((isScanning) {
-      this.isScanning = isScanning;
-    });
   }
 
   // Stop scanning for devices
   Future<void> stopScan() async {
+    isScanning = false;
     await FlutterBluePlus.stopScan();
   }
 
